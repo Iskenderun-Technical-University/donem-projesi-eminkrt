45af36326eecce571e631b7efe4f31de25b0a1cb
eminkrt <118981996+eminkrt@users.noreply.github.com>
2023-05-27 22:29:16 +0300

    Add files via upload

---
+++ Sürücü Kursu Otomasyonu/Surucu Kursuna Hosgeldiniz.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sürücü_Kursu_Otomasyonu
+{
+    public partial class Surucu_Kursuna_Hosgeldiniz : Form
+    {
+        public Surucu_Kursuna_Hosgeldiniz()
+        {
+            InitializeComponent();
+        }
+        public static string f = "";
+            
+        public static int fiyat;
+        private void button1_Click(object sender, EventArgs e)
+
+        {
+            f = textBox2.Text;
+            this.Hide();
+            Form2 a = new Form2();
+            a.Show();
+
+        }
+
+        private void Surucu_Kursuna_Hosgeldiniz_Load(object sender, EventArgs e)
+        {
+            Sınıf.griddoldur(dataGridView1,"select * from ehliyet");
+            dataGridView1.Columns[0].Visible = false;
+
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textBox1.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            textBox2.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            textBox3.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Sürücü Kursu Otomasyonu/Şifre Güncelleme.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+
+namespace Sürücü_Kursu_Otomasyonu
+{
+    public partial class Şifre_Güncelleme : Form
+    {
+        SqlConnection con;
+
+        SqlDataReader dr;
+        SqlCommand cmd;
+        //public static string SqlCon = @"Data Source=localhost\SQLEXPRESS;Initial Catalog = Surucu Kursu; Integrated Security = True";
+        public int sonuc = 0;
+        public Şifre_Güncelleme()
+        {
+            InitializeComponent();
+        }
+
+        private void Şifre_Güncelleme_Load(object sender, EventArgs e)
+        {
+
+            label5.Name = "";
+            CaptchaSayıDegistir();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label5.Text = "";
+            if (textBox4.Text == sonuc.ToString())
+            {
+                if (textBox2.Text == textBox3.Text)
+                {
+                    eskisifre();
+
+                }
+                else
+                {
+                    label6.Text = "Eski Ve Yeni Şifreniz Aynı Değildir";
+                    CaptchaSayıDegistir();
+                }
+            }
+            else
+            {
+                label6.Text = "Captcha Hatalı Girildi ";
+                CaptchaSayıDegistir();
+            }
+        }
+        public bool eskisifre()
+        {
+            string sorgu = "select sifre from tbl_login where kullanıcı=@user and sifre=@pass";
+            con = new SqlConnection(Sınıf.BAG());
+            cmd = new SqlCommand(sorgu, con);
+            cmd.Parameters.AddWithValue("@user", Form1.kullaniciSession);
+            cmd.Parameters.AddWithValue("@pass", Sınıf.MD5sifreleme(textBox1.Text));
+
+            con.Open();
+            dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                string sql = "update tbl_login set sifre='" + Sınıf.MD5sifreleme(textBox2.Text) + "'";
+                Sınıf.kom(sql);
+                MessageBox.Show("Şifreniz Başarıyla Değiştirilmiştir");
+            }
+            else
+            {
+                label6.Text = "Eski Şifreniz Hatalıdır";
+                CaptchaSayıDegistir();
+            }
+            con.Close();
+            return true;
+        }
+        public void CaptchaSayıDegistir()
+        {
+            Random sayı = new Random();
+            int bir = sayı.Next(0, 50);
+            int iki = sayı.Next(0, 50);
+            sonuc = bir + iki;
+            label4.Text = bir.ToString() + "+" + iki.ToString() + "=";
+            textBox4.Clear();
+        }
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            label5.Text = "";
+            if (textBox4.Text == sonuc.ToString())
+            {
+                if (textBox2.Text == textBox3.Text)
+                {
+                    eskisifre();
+
+                }
+                else
+                {
+                    label6.Text = "Eski Ve Yeni Şifreniz Aynı Değildir";
+                    CaptchaSayıDegistir();
+                }
+            }
+            else
+            {
+                label6.Text = "Captcha Hatalı Girildi ";
+                CaptchaSayıDegistir();
+            }
+        }
+    }
+}
+    
+
